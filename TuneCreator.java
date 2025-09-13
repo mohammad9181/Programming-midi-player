3d7fa25f1d63a6325d30461ff6e40cceb2319156
Mohammad <2583045@edu.vaniercollege.qc.ca>
2025-09-12 20:27:50 -0400

    Added TuneCreator

---
+++ TuneCreator.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class TuneCreator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TuneCreator
+{
+    // instance variables - replace the example below with your own
+    private MidiPlayer player;
+
+    /**
+     * Constructor for objects of class TuneCreator
+     */
+    public TuneCreator()
+    {
+        MidiPlayer player = new MidiPlayer();
+        this.player = player;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void CreateAndPlay()
+    {
+        SimpleTune tune  = new SimpleTune();
+        
+        tune.addNote("A", 10);
+        tune.addNote("B", 10);
+        tune.addNote("C", 10);
+        tune.addNote("A", 10);
+        tune.addNote("B", 10);
+        tune.addNote("C", 10);
+        
+        player.playTune(tune);
+    }
+}
